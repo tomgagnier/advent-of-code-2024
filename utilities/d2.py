d325b5baa9bab8154161548caa9bb91248ed3712
Tom Gagnier <tomgagnier@gmail.com>
2024-12-08 20:45:59 -0500

    Unit test d2

--- utilities/d2.py
+++ utilities/d2.py
@@ -2,6 +2,49 @@ import textwrap
 from dataclasses import dataclass
 from typing import Any, Iterator, Self, Callable
 
+NEIGHBOR_OFFSETS = [
+    (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)
+]
+
+
+@dataclass(frozen=True, eq=True)
+class Coordinate:
+    ij: tuple[int, int]
+
+    @classmethod
+    def from_tuples(cls, *tuples: tuple[int, int]) -> list[Self]:
+        return [Coordinate(t) for t in tuples]
+
+    @property
+    def i(self) -> int:
+        return self.ij[0]
+
+    @property
+    def j(self) -> int:
+        return self.ij[1]
+
+    def __getitem__(self, index: int) -> int:
+        return self.ij[index]
+
+    def __add__(self, other: Self | tuple[int, int]) -> Self:
+        return Coordinate((self[0] + other[0], self[1] + other[1]))
+
+    def __sub__(self, other: Self | tuple[int, int]) -> Self:
+        return Coordinate((self[0] - other[0], self[1] - other[1]))
+
+    def __repr__(self) -> str:
+        return str(self.ij)
+
+    def __eq__(self, other: Self | tuple[int, int]) -> bool:
+        return isinstance(other, Coordinate) and self.ij == other.ij or self.ij == other
+
+    def translate(self, coordinates: list[Self | tuple[int, int]]) -> Iterator[Self]:
+        return map(lambda c: c + self, coordinates)
+
+    def neighbors(self) -> Iterator[Self]:
+        return map(lambda c: self + c, NEIGHBOR_OFFSETS)
+
+
 
 @dataclass(frozen=True)
 class Matrix:
@@ -15,22 +58,27 @@ class Matrix:
     def from_multiline_str(cls, text: str) -> Self:
         return Matrix.from_lines(textwrap.dedent(text).strip().split('\n'))
 
-    def __getitem__(self, coordinates: tuple[int, int]) -> str:
-        return self.elements[self.max_j - 1 - coordinates[1]][coordinates[0]]
+    @classmethod
+    def from_file(cls, input_file):
+        with (open(input_file, 'r') as file):
+            return Matrix.from_lines(file.readlines())
 
-    def __setitem__(self, coordinates: tuple[int, int], value: str) -> None:
-        self.elements[self.max_j - 1 - coordinates[1]][coordinates[0]] = value
+    def __getitem__(self, coordinate: Coordinate | tuple[int, int]) -> str:
+        return self.elements[self.max_j - 1 - coordinate[1]][coordinate[0]]
+
+    def __setitem__(self, coordinate: Coordinate | tuple[int, int], value: str) -> None:
+        self.elements[self.max_j - 1 - coordinate[1]][coordinate[0]] = value
 
     def __repr__(self) -> str:
         return '\n'.join([''.join(self.elements[j]) for j in range(self.max_j)])
 
-    def __iter__(self) -> Iterator[tuple[int, int]]:
+    def __iter__(self) -> Iterator[Coordinate]:
         for j in range(self.max_j):
             for i in range(self.max_i):
-                yield i, j
+                yield Coordinate((i, j))
 
-    def __contains__(self, coordinates: tuple[int, int]) -> bool:
-        return 0 <= coordinates[0] < self.max_i and 0 <= coordinates[1] < self.max_j
+    def __contains__(self, coordinate: Coordinate) -> bool:
+        return 0 <= coordinate[0] < self.max_i and 0 <= coordinate[1] < self.max_j
 
     @property
     def max_i(self) -> int:
@@ -40,16 +88,13 @@ class Matrix:
     def max_j(self) -> int:
         return len(self.elements)
 
-    def get(self, coordinates: tuple[int, int], default: str | None = None) -> Any:
-        return self[coordinates] if coordinates in self else default
+    def get(self, coordinate: Coordinate | tuple[int, int], default: str | None = None) -> Any:
+        return self[coordinate] if coordinate in self else default
 
-    def find(self, predicate: Callable[[str], bool]) -> tuple[int, int] | None:
-        matches = (coordinate for coordinate in self if predicate(self[coordinate]))
-        return next(matches, None)
+    def find(self, str_predicate: Callable[[str], bool]) -> Coordinate | None:
+        matches = (coordinate for coordinate in self if str_predicate(self[coordinate]))
+        first = next(matches, None)
+        return first
 
-    def neighbors(self, coordinates: tuple[int, int]) -> list[tuple[int, int]]:
-        i, j = coordinates
-        return [neighbor for neighbor in [
-            (i - 1, j - 1), (i - 1, j), (i - 1, j + 1), (i, j + 1),
-            (i + 1, j + 1), (i + 1, j), (i + 1, j - 1), (i, j - 1)
-        ] if neighbor in self]
+    def neighbors(self, coordinate: Coordinate) -> Iterator[Coordinate]:
+        return (neighbor for neighbor in coordinate.neighbors() if neighbor in self)
--- utilities/test_d2.py
+++ utilities/test_d2.py
@@ -1,6 +1,7 @@
 import unittest
 
 import d2
+from utilities.d2 import Coordinate
 
 matrix = d2.Matrix.from_multiline_str(
     """
@@ -12,7 +13,19 @@ matrix = d2.Matrix.from_multiline_str(
 )
 
 
-class MyTestCase(unittest.TestCase):
+class TestD2(unittest.TestCase):
+
+    def test_coordinate(self):
+        self.assertEqual(2, Coordinate((2, 1)).i)
+        self.assertEqual(2, Coordinate((2, 1))[0])
+        self.assertEqual(1, Coordinate((2, 1)).j)
+        self.assertEqual(1, Coordinate((2, 1))[1])
+        self.assertEqual(Coordinate((5, 6)), Coordinate((2, 1)) + Coordinate((3, 5)))
+        self.assertEqual(Coordinate((-1, -4)), Coordinate((2, 1)) - Coordinate((3, 5)))
+        self.assertEqual('(1, 8)', str(Coordinate((1, 8))))
+        self.assertEqual([Coordinate((2, 3)), Coordinate((5, 4))],
+                         list(Coordinate((1, -2)).translate(
+                             [Coordinate((1, 5)), Coordinate((4, 6))])))
 
     def test_max(self):
         self.assertEqual(3, matrix.max_i)
@@ -37,22 +50,26 @@ class MyTestCase(unittest.TestCase):
         self.assertIsNone(matrix.get((5, 5)))
 
     def test_iter(self):
-        self.assertEqual([
-            (0, 0), (1, 0), (2, 0),
-            (0, 1), (1, 1), (2, 1),
-            (0, 2), (1, 2), (2, 2),
-            (0, 3), (1, 3), (2, 3),
-        ], list(matrix))
+        self.assertEqual(
+            [d2.Coordinate(t) for t in
+             [(0, 0), (1, 0), (2, 0),
+              (0, 1), (1, 1), (2, 1),
+              (0, 2), (1, 2), (2, 2),
+              (0, 3), (1, 3), (2, 3)]],
+            list(matrix))
 
     def test_find(self):
-        self.assertEqual((0, 0), matrix.find(lambda x: x == 'j'))
-        self.assertEqual((1, 2), matrix.find(lambda x: x == 'e'))
         self.assertIsNone(matrix.find(lambda x: x == 'z'))
+        coordinate = Coordinate((1, 2))
+        find = matrix.find(lambda x: x == 'e')
+        self.assertEqual(coordinate, find)
+        self.assertEqual(Coordinate((0, 0)), matrix.find(lambda x: x == 'j'))
 
     def test_neighbors(self):
-        self.assertEqual([(0, 1), (1, 1), (1, 0)], matrix.neighbors((0, 0)))
-        self.assertEqual([(0, 2), (0, 3), (2, 3), (2, 2), (1, 2)],
-                         matrix.neighbors((1, 3)))
+        self.assertEqual(Coordinate.from_tuples((0, 1), (1, 1), (1, 0)),
+                         list(matrix.neighbors(Coordinate((0, 0)))))
+        self.assertEqual(Coordinate.from_tuples((0, 2), (0, 3), (2, 3), (2, 2), (1, 2)),
+                         list(matrix.neighbors(Coordinate((1, 3)))))
 
 
 if __name__ == '__main__':
